bb7957483d090bd43243597d4146a7915c1d4a3e
Chamelsqr999 <lequockyanh99@gmail.com>
2023-11-01 19:51:34 +0700

    Search photo ver2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import './App.css';
 
 const App = () => {
-  const [query, setQuery] = useState('');
-  const [photos, setPhotos] = useState([]);
+  const [img, setImg] = useState('');
+  const [res, setRes] = useState([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
 
-  const accessKey = "AmDmbh53KjNSXq2arbdupaJrQ69wNeTg5R8Q8MI_Y6Y"; 
-  const apiUrl = 'https://api.unsplash.com/search/photos'; 
+  const accessKey = 'IHDyvwgZFV3Ip2k3rbU6fuvXPNM83AcPMazOesF94fA'; 
+  const perPage = 20;
 
-  const fetchPhotos = async () => {
+  const fetchRequest = async (pageNum) => {
     try {
       setLoading(true);
-      const response = await axios.get(apiUrl, {
-        params: {
-          query,
-          page,
-          per_page: 10,
-          client_id: accessKey,
-        },
-      });
-      setPhotos([...photos, ...response.data.results]);
-      setPage(page + 1);
+      const response = await fetch(
+        `https://api.unsplash.com/search/photos?page=${pageNum}&query=${img.toLowerCase()}&client_id=${accessKey}&per_page=${perPage}`
+      );
+      const data = await response.json();
+      const result = data.results;
+      if (result.length === 0) {
+        setHasMore(false);
+      } else {
+        setRes([...res, ...result]);
+        setPage(pageNum + 1);
+      }
     } catch (error) {
       console.error(error);
     } finally {
@@ -32,37 +33,55 @@ const App = () => {
     }
   };
 
+  useEffect(() => {
+    fetchRequest(page);
+  }, [page]);
+
   const handleSearch = () => {
-    setPhotos([]);
     setPage(1);
-    fetchPhotos();
+    setRes([]);
+    setHasMore(true);
+    fetchRequest(1);
   };
 
   return (
-    <div className="App">
-      <div className="search-bar">
-        <input
-          type="text"
-          placeholder="Search for photos..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <button onClick={handleSearch}>Search</button>
-      </div>
-      {loading && <p>Loading...</p>}
-      <InfiniteScroll
-        dataLength={photos.length}
-        next={fetchPhotos}
-        hasMore={true}
-        loader={<h4>Loading...</h4>}
-        endMessage={<p>No more photos to load</p>}
-      >
-        <div className="photo-list">
-          {photos.map((photo) => (
-            <img key={photo.id} src={photo.urls.small} alt={photo.alt_description} />
-          ))}
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12 d-flex justify-content-center align-items-center input">
+          <input
+            className="form-control form-control-sm py-1 fs-4 text-capitalize border border-dark"
+            type="text"
+            placeholder="Search Anything..."
+            value={img}
+            onChange={(e) => setImg(e.target.value)}
+          />
+          <button
+            type="button"
+            onClick={handleSearch}
+            className="btn bg-dark text-white fs-3 mx-3"
+          >
+            Search
+          </button>
         </div>
-      </InfiniteScroll>
+        <div className="col-12 d-flex justify-content-evenly flex-wrap">
+          <InfiniteScroll
+            dataLength={res.length}
+            next={() => fetchRequest(page)}
+            hasMore={hasMore}
+            loader={<h4>Loading...</h4>}
+            endMessage={<p>No more photos to load</p>}
+          >
+            {res.map((val) => (
+              <img
+                key={val.id}
+                className="col-3 img-fluid img-thumbnail"
+                src={val.urls.small}
+                alt={val.alt_description || 'Image'}
+              />
+            ))}
+          </InfiniteScroll>
+        </div>
+      </div>
     </div>
   );
 };
